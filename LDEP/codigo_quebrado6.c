a93e707a283c5048f48cdd946740707a46c1cae5
caiofraga123 <caiofcintra@gmail.com>
2024-04-01 16:29:02 -0300

    01/04

---
+++ LDEP/codigo_quebrado6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	
+	int l = 0;
+	int c = 0;
+	int i = 0;
+	int j = 0;
+	int soma = 0; 
+	int atual = 0;
+	int total = 0;
+
+	scanf("%d %d", &l, &c);
+
+	
+	for (i = 0; i<c; i++){
+		for(j = 0; j<l; j++){
+			scanf("%d", &atual);
+			soma += atual;
+		}
+		total += soma;
+		printf("%d\n", soma);
+		soma = 0;
+	}
+
+	printf("%d\n", total);
+
+	return 0;
+}
