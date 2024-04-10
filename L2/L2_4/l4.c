95d277fe133a6a735d6062374034a7061f30f663
caiofraga123 <caiofcintra@gmail.com>
2024-04-10 16:27:27 -0300

    10/04

--- L2/L2_4/l4.c
+++ L2/L2_4/l4.c
@@ -1,23 +1,33 @@
-    #include <stdio.h>
+#include <stdio.h>
 
-    int main(){
-        int verdade = 1;
-        int numero = 0;
-        int cont = 0;
-        int soma = 0;
-        float media = 0;
+int main(){
 
-        while(verdade){
-            scanf("%d ", &numero);
-            if (numero == 0){
-                verdade = 0;
-            }
-            cont += 1;
-            soma += numero;
-            media = soma/cont;
-            printf("%d  %6.f ", numero, media);
+    int verdade = 1;
+    int numero = 0;
+    float cont = 0;
+    int soma = 0;
+    float media = 0;
+    int maior = 0;
+
+    while(verdade){
+        scanf("%d", &numero);
+        if (numero == 0){
+            verdade = 0;
         }
-
-
-        return 0;
+        
+        if (verdade == 0){
+            break;
+        }
+        
+        if (numero >= maior){
+            maior = numero;
+        }
+        
+        cont += 1;
+        soma += numero;
+        media = soma/cont;
+        printf("%d  %.6f\n", maior, media);
     }
+    
+    return 0;
+}
